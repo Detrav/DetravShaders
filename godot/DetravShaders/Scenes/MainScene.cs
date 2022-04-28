9a74bd73d58103d565dd307d42cc314513c274d1
Detrav <witalyezep@gmail.com>
2022-04-29 00:33:29 +0300

    * rework sheaderitem for mobile view

--- godot/DetravShaders/Scenes/MainScene.cs
+++ godot/DetravShaders/Scenes/MainScene.cs
@@ -5,21 +5,51 @@ using System;
 public class MainScene : Control
 {
     private Shader[] lastMaterials;
+    private GridContainer grid;
+    private int lastColumns;
+    private float? updateTime;
 
     [Export]
     public Shader[] ShaderMaterials { get; set; } = new Shader[0];
+
+
 
     [Export]
     public PackedScene ShaderItem { get; set; }
     public override void _Ready()
     {
+        grid = GetNode<GridContainer>("ScrollContainer/GridContainer");
         UpdateMaterials();
+        lastColumns = grid.Columns;
     }
 
     public override void _Process(float delta)
     {
         base._Process(delta);
 
+        if(lastColumns != grid.Columns)
+        {
+            lastMaterials = null;
+            lastColumns = grid.Columns;
+        }
+
+        if (updateTime != null)
+        {
+            updateTime -= delta;
+            if(updateTime < 0)
+            {
+                if(RectSize.x > RectSize.y - 100)
+                {
+                    grid.Columns = 3;
+                }
+                else
+                {
+                    grid.Columns = 1;
+                }
+                lastMaterials = null;
+                updateTime = null;
+            }
+        }
 
         UpdateMaterials();
 
@@ -35,20 +65,40 @@ public class MainScene : Control
 
         if (ShaderMaterials != lastMaterials)
         {
-            var grid = GetNode<GridContainer>("ScrollContainer/GridContainer");
 
             foreach (Node ch in grid.GetChildren())
             {
                 ch.QueueFree();
             }
 
+            float width = RectSize.x;
+
+            GD.Print(width);
+
+            if (grid.Columns >= 1)
+            {
+                width -= 10 * grid.Columns + 2 ;
+                width /= grid.Columns;
+                GD.Print(width);
+            }
+
+            GD.Print(width);
+
+
             foreach (var shader in ShaderMaterials)
             {
                 var item = ShaderItem.Instance<ShaderItem>();
+                item.RectMinSize = new Vector2(width, 0);
+                item.RectSize = new Vector2(width, 0);
                 item.ItemShader = shader;
                 grid.AddChild(item);
             }
             lastMaterials = ShaderMaterials;
         }
+    }
+
+    public void _on_MainScene_resized()
+    {
+        updateTime = 0.15f;
     }
 }
--- godot/DetravShaders/Scenes/ShaderItem.cs
+++ godot/DetravShaders/Scenes/ShaderItem.cs
@@ -9,6 +9,8 @@ public class ShaderItem : VBoxContainer
     [Export]
     public Shader ItemShader { get; set; }
 
+    private float lastWidth = 0;
+
     public override void _Ready()
     {
         colorRect = GetNode<ColorRect>("ColorRect");
@@ -18,5 +20,15 @@ public class ShaderItem : VBoxContainer
         };
         label = GetNode<Label>("Label");
         label.Text = System.IO.Path.GetFileNameWithoutExtension(ItemShader?.ResourcePath ?? "None");
+    }
+
+    public override void _Process(float delta)
+    {
+        base._Process(delta);
+        if (Mathf.Abs(colorRect.RectSize.x - lastWidth) > 0.01)
+        {
+            colorRect.RectMinSize = new Vector2(0, colorRect.RectSize.x);
+            lastWidth = colorRect.RectSize.x;
+        }
     }
 }
